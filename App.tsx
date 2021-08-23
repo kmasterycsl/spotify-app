736fda44e7738f9902f60718c4e652681693bd80
Khanh Tran <khanh.tran@canalcircle.com>
2021-08-23 21:46:18 +0700

    add dynamic env

--- App.tsx
+++ App.tsx
@@ -1,14 +1,34 @@
+import { ApolloClient, ApolloProvider, gql, InMemoryCache } from "@apollo/client";
+import Constants from 'expo-constants';
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Player from "./src/components/Player";
-import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
 
 // Initialize Apollo Client
 const apolloClient = new ApolloClient({
-  uri: "http://localhost:3000/graphql",
+  uri: Constants?.manifest?.extra?.GRAPHQL_ENDPOINT,
   cache: new InMemoryCache(),
 });
+
+const SECTIONS_QUERY = gql`
+query Sections {
+  getArtists(page: 1, limit: 5) {
+    items {
+      avatarImage {
+        type
+      }
+    }
+    meta {
+      itemCount
+      currentPage
+      totalPages
+      totalItems
+    }
+  }
+}
+`
 
 export default function App() {
   return (

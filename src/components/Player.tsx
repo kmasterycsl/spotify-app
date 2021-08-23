eb876e8f8590191438e03f9b10e1974947d3c54b
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-23 21:10:41 +0700

    add working graphql

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,14 +1,35 @@
 import * as React from "react";
 import { Text, View, StyleSheet, Button } from "react-native";
 import { Audio } from "expo-av";
+import { gql, useQuery } from "@apollo/client";
+
+const SECTIONS_QUERY = gql`
+query Sections {
+  getArtists(page: 1, limit: 5) {
+    items {
+      avatarImage {
+        type
+      }
+    }
+    meta {
+      itemCount
+      currentPage
+      totalPages
+      totalItems
+    }
+  }
+}
+`
 
 export default function Player() {
+  const { data, loading, error } = useQuery(SECTIONS_QUERY);
+
   const [sound, setSound] = React.useState<Audio.Sound>();
 
   async function playSound() {
     console.log("Loading Sound");
     const { sound } = await Audio.Sound.createAsync(
-      require("../assets/demo.mp3")
+      require("../../assets/demo.mp3")
     );
     setSound(sound);
 
@@ -25,5 +46,5 @@ export default function Player() {
       : undefined;
   }, [sound]);
 
-  return <Button title="Play Sound" onPress={playSound} />;
+  return <Button title={"Play Sound" + data?.getArtists?.items?.[0]?.avatarImage?.type} onPress={playSound} />;
 }

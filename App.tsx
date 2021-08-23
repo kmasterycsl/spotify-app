5c52938c12aa02a7783106ba3e26cfc8593d8256
Khanh Tran <khanh.tran@canalcircle.com>
2021-08-23 21:10:41 +0700

    add working graphql

--- App.tsx
+++ App.tsx
@@ -1,23 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Player from './components/Player';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Player from "./src/components/Player";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+// Initialize Apollo Client
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:3000/graphql",
+  cache: new InMemoryCache(),
+});
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-      <Player />
-    </View>
+    <ApolloProvider client={apolloClient}>
+      <View style={styles.container}>
+        <Text>Open up App.tsx to start working on your app!</Text>
+        <StatusBar style="auto" />
+        <Player />
+      </View>
+    </ApolloProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });

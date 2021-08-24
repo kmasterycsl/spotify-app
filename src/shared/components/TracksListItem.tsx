abf833b019a7ce09733b32d77dd8c98f8589a737
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-24 23:08:14 +0700

    temp

--- src/shared/components/TracksListItem.tsx
+++ src/shared/components/TracksListItem.tsx
@@ -1,11 +1,25 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { Track } from "../../types/graphql";
+import HStack from "./HStack";
+import VStack from "./VStack";
 
 export default function TracksListItem({ track }: { track: Track }) {
-    return (
-        <View>
-            <Text>{track.name}</Text>
-        </View>
-    )
+  return (
+    <HStack>
+      <Image
+        style={{
+          width: 50,
+          height: 50,
+        }}
+        source={{
+          uri: `https://picsum.photos/${50}/${50}?random=${Math.random()}&grayscale&blur=2`,
+        }}
+      ></Image>
+      <VStack>
+        <Text>{track.name}</Text>
+        <Text>{Math.floor(Math.random() * 100000)}</Text>
+      </VStack>
+    </HStack>
+  );
 }

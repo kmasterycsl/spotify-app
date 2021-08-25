fdb2966bec886ca5208dd8d8fe535861e80cc8e4
Khanh Tran <tranduykhanhyc@gmail.com>
2021-08-25 23:31:28 +0700

    update

--- src/shared/components/TracksList.tsx
+++ src/shared/components/TracksList.tsx
@@ -1,3 +1,4 @@
+import { Box } from "native-base";
 import React from "react";
 import { View } from "react-native";
 import { Track } from "../../types/graphql";
@@ -8,14 +9,13 @@ export default function TracksList({ tracks }: { tracks: Track[] }) {
   return (
     <View>
       {tracks.map((track, index) => (
-        <>
+        <Box key={track.id}>
           <TracksListItem
-            key={track.id}
             track={track}
             index={index}
           ></TracksListItem>
           {index !== tracks.length - 1 && <VerticalPadding />}
-        </>
+        </Box>
       ))}
     </View>
   );

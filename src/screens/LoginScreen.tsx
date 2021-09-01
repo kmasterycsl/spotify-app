f273b88ad9cbfb88bce417813a569db36fd0fd3c
Khanh Tran <tranduykhanhyc@gmail.com>
2021-09-02 00:53:56 +0700

    ok

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,7 +1,8 @@
-import { Button } from "native-base";
-import React from "react";
+import { ResponseType } from "expo-auth-session";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
+import { Button } from "native-base";
+import React from "react";
 
 WebBrowser.maybeCompleteAuthSession();
 
@@ -11,6 +12,7 @@ export default function LoginScreen() {
     iosClientId: "1046589284135-jlm6k2ob8opn14vppvjh6sueqmk1e7sj.apps.googleusercontent.com",
     androidClientId: "1046589284135-jlm6k2ob8opn14vppvjh6sueqmk1e7sj.apps.googleusercontent.com",
     webClientId: "1046589284135-jlm6k2ob8opn14vppvjh6sueqmk1e7sj.apps.googleusercontent.com",
+    responseType: ResponseType.IdToken,
   });
 
   React.useEffect(() => {

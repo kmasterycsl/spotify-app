1c8d6646e1492ce4f17ca8f7edf84e3a66c11179
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-03 22:22:10 +0700

    update

--- App.tsx
+++ App.tsx
@@ -1,34 +1,50 @@
 import { ApolloProvider } from "@apollo/client";
 import { NavigationContainer } from "@react-navigation/native";
-import AppLoading from "expo-app-loading";
+import * as SplashScreen from "expo-splash-screen";
 import { NativeBaseProvider } from "native-base";
-import React from "react";
+import React, { useCallback } from "react";
+import { View } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import { apolloClient } from "./src/config/apollo";
-import useCustomFonts from "./src/hooks/useCustomFonts";
-import useDebugStore from "./src/hooks/useDebugStore";
+import theme, { colorModeManager } from "./src/config/theme";
+import useAppStartup from "./src/hooks/useAppStartup";
 import MainStack from "./src/routers/main-stack";
 import GlobalToast from "./src/shared/components/GlobalToast";
-import theme, { colorModeManager } from "./src/config/theme";
+
+SplashScreen.preventAutoHideAsync().catch(() => {});
 
 export default function App() {
-  useDebugStore();
-  const fontsLoaded = useCustomFonts();
-
-  if (!fontsLoaded) {
-    return <AppLoading />;
-  }
-
   return (
     <NativeBaseProvider theme={theme} colorModeManager={colorModeManager}>
       <ApolloProvider client={apolloClient}>
         <NavigationContainer>
-          <SafeAreaProvider>
-            <MainStack />
-            <GlobalToast />
-          </SafeAreaProvider>
+          <AppBoostraper />
         </NavigationContainer>
       </ApolloProvider>
     </NativeBaseProvider>
+  );
+}
+
+function AppBoostraper() {
+  const appIsReady = useAppStartup();
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  return (
+    <View
+      style={{
+        flex: 1,
+      }}
+      onLayout={onLayoutRootView}
+    >
+      <SafeAreaProvider>
+        <MainStack />
+        <GlobalToast />
+      </SafeAreaProvider>
+    </View>
   );
 }

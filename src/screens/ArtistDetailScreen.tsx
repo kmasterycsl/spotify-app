0b47113192c9c9f40c329bb170c5540f855af96d
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-14 23:13:07 +0700

    refactor

--- src/screens/ArtistDetailScreen.tsx
+++ src/screens/ArtistDetailScreen.tsx
@@ -1,19 +1,18 @@
-import { gql, useQuery } from "@apollo/client";
+import { useQuery } from "@apollo/client";
 import { Ionicons } from "@expo/vector-icons";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
-import { Box, HStack, Icon, IconButton, Text, VStack } from "native-base";
+import { Box, HStack, Icon, Text, VStack } from "native-base";
 import React, { useEffect, useMemo, useState } from "react";
-import { Dimensions } from "react-native";
+import { Dimensions, StyleSheet } from "react-native";
 import Animated, {
-    useSharedValue,
+    useAnimatedProps,
     useAnimatedScrollHandler,
     useAnimatedStyle,
-    useAnimatedProps,
+    useSharedValue,
 } from "react-native-reanimated";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 import FullWidthSquareImage from "../shared/components/FullWidthSquareImage";
 import HorizontalPadding, {
-    DEFAULT_HORIZONTAL_PADDING,
     _DEFAULT_HORIZONTAL_PADDING,
 } from "../shared/components/HorizontalPadding";
 import PlayerBar from "../shared/components/PlayerBar";
@@ -40,40 +39,38 @@ export default function ArtistDetailScreen() {
         totalPages: Infinity,
     });
     const [loading, setLoading] = useState(false);
-    const { data, error, refetch, fetchMore } = useQuery<Query>(GET_ARTIST_BY_ID_QUERY, {
+    const { data, refetch, fetchMore } = useQuery<Query>(GET_ARTIST_BY_ID_QUERY, {
         variables: {
             id: params.artistId,
             page: 1,
         },
     });
-    if (error) {
-        console.error(error);
-    }
+
     useEffect(() => {
         refetch();
     }, [params.artistId]);
 
-    const translationY = useSharedValue(0);
+    const scrollOffsetY = useSharedValue(0);
 
     const scrollHandler = useAnimatedScrollHandler(event => {
-        translationY.value = event.contentOffset.y;
+        scrollOffsetY.value = event.contentOffset.y;
     });
 
-    const stylez = useAnimatedStyle(() => {
+    const coverImgStyle = useAnimatedStyle(() => {
         return {
-            opacity: 1 - translationY.value / screenWidth,
+            opacity: 1 - scrollOffsetY.value / screenWidth,
         };
     });
 
-    const styleHeader = useAnimatedStyle(() => {
+    const hiddenHeaderStyle = useAnimatedStyle(() => {
         return {
-            opacity: (translationY.value * 1.4) / screenWidth,
+            opacity: (scrollOffsetY.value * 1.4) / screenWidth,
         };
     });
 
-    const backIconBg = useAnimatedProps(() => {
+    const backIconStyle = useAnimatedProps(() => {
         return {
-            backgroundColor: translationY.value / screenWidth > 0.5 ? "transparent" : "gray",
+            backgroundColor: scrollOffsetY.value / screenWidth > 0.5 ? "transparent" : "gray",
         };
     });
 
@@ -108,7 +105,7 @@ export default function ArtistDetailScreen() {
             });
     };
 
-    const headerComponent = useMemo(
+    const ListHeaderComponent = useMemo(
         () => (
             <VStack>
                 <HorizontalPadding style={{ backgroundColor: "transparent" }}>
@@ -130,50 +127,22 @@ export default function ArtistDetailScreen() {
     );
 
     return data?.artist ? (
-        <SafeAreaView style={{ flex: 1, position: "relative" }} edges={["bottom"]}>
-            <Animated.View
-                style={[
-                    {
-                        position: "absolute",
-                        width: "100%",
-                        opacity: 0,
-                        zIndex: 3,
-                    },
-                    styleHeader,
-                ]}
-            >
+        <SafeAreaView style={styles.container} edges={["bottom"]}>
+            {/* Hidden header */}
+            <Animated.View style={[styles.hiddenHeaderContainer, hiddenHeaderStyle]}>
                 <HStack
-                    style={{ paddingTop: insets.top }}
-                    px={DEFAULT_HORIZONTAL_PADDING}
-                    pb={DEFAULT_HORIZONTAL_PADDING}
-                    justifyContent="center"
-                    width="100%"
-                    position="absolute"
+                    paddingTop={insets.top}
+                    style={styles.hiddenHeader}
                     bg={(data?.artist?.coverImage?.meta as ImageMeta)?.dominantColor}
                 >
                     <Text>{data.artist.name}</Text>
                 </HStack>
             </Animated.View>
 
-            <Box style={{ position: "absolute", zIndex: 3 }}>
-                <Animated.View
-                    style={[
-                        {
-                            position: "absolute",
-                            top: insets.top,
-                            left: _DEFAULT_HORIZONTAL_PADDING,
-                        },
-                    ]}
-                >
-                    <Animated.View
-                        style={[
-                            {
-                                backgroundColor: "gray",
-                                borderRadius: 100,
-                            },
-                            backIconBg,
-                        ]}
-                    >
+            {/* Hidden back icon */}
+            <Box style={styles.backIconContainer}>
+                <Animated.View style={[styles.backIconInner, { top: insets.top }]}>
+                    <Animated.View style={[styles.backIcon, backIconStyle]}>
                         <Icon
                             onPress={goBack}
                             size={5}
@@ -184,36 +153,67 @@ export default function ArtistDetailScreen() {
                 </Animated.View>
             </Box>
 
-            <Animated.View style={[{ position: "absolute", zIndex: 0 }, stylez]}>
+            {/* Cover image */}
+            <Animated.View style={[{ position: "absolute", zIndex: 0 }, coverImgStyle]}>
                 <FullWidthSquareImage
                     url={data?.artist?.coverImage?.meta?.source}
                 ></FullWidthSquareImage>
             </Animated.View>
 
+            {/* Tracks list */}
             <TracksList
                 styles={{
-                    listContainer: {
-                        position: "absolute",
-                        paddingTop: screenWidth - 40,
-                        top: 0,
-                        left: 0,
-                        height: "100%",
-                        zIndex: 2,
-                    },
-                    footer: {
-                        paddingBottom: screenWidth + 80 - 40, // + playerbar
-                    },
+                    listContainer: styles.tracksListContainer,
+                    footer: styles.tracksListFooter,
                 }}
                 onScroll={scrollHandler}
-                headerComponent={headerComponent}
+                headerComponent={ListHeaderComponent}
                 isFinished={paginationMeta.currentPage >= paginationMeta.totalPages}
                 isLoading={loading}
                 onLoadMore={onLoadMore}
                 tracks={data.artist.tracks.items}
             />
-            <Box style={{ position: "absolute", zIndex: 4, bottom: insets.bottom, width: "100%" }}>
+
+            {/* Player bar */}
+            <Box style={styles.playerBarContainer} bottom={insets.bottom}>
                 <PlayerBar />
             </Box>
         </SafeAreaView>
     ) : null;
 }
+
+const styles = StyleSheet.create({
+    container: { flex: 1, position: "relative" },
+    hiddenHeaderContainer: {
+        position: "absolute",
+        width: "100%",
+        opacity: 0,
+        zIndex: 3,
+    },
+    hiddenHeader: {
+        paddingLeft: _DEFAULT_HORIZONTAL_PADDING,
+        paddingRight: _DEFAULT_HORIZONTAL_PADDING,
+        paddingBottom: _DEFAULT_HORIZONTAL_PADDING,
+        justifyContent: "center",
+        width: "100%",
+        position: "absolute",
+    },
+    backIconContainer: { position: "absolute", zIndex: 3 },
+    backIconInner: { position: "absolute", left: _DEFAULT_HORIZONTAL_PADDING },
+    backIcon: {
+        backgroundColor: "gray",
+        borderRadius: 100,
+    },
+    tracksListContainer: {
+        position: "absolute",
+        paddingTop: screenWidth - 40,
+        top: 0,
+        left: 0,
+        height: "100%",
+        zIndex: 2,
+    },
+    tracksListFooter: {
+        paddingBottom: screenWidth + 80 - 40,
+    },
+    playerBarContainer: { position: "absolute", zIndex: 4, width: "100%" },
+});

3007f830a988388c08a1565e93b26e8f70ba8f9a
Khanh Tran <khanh.tran@canalcircle.com>
2021-09-15 22:58:32 +0700

    add basic home

--- src/routers/main-tab.tsx
+++ src/routers/main-tab.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import HomeStackScreen from "./home-stack";
 import LibraryStackScreen from "./library-stack";
+import { useCommonStore } from "../store/common.store";
 
 function SettingsScreen() {
     return (
@@ -16,11 +17,13 @@ function SettingsScreen() {
 const Tab = createBottomTabNavigator();
 
 export default function MainTab() {
+    const currentUser = useCommonStore(state => state.currentUser);
+
     return (
         <Tab.Navigator>
-            <Tab.Screen name="Home" component={HomeStackScreen} />
+            <Tab.Screen name="HomeStack" component={HomeStackScreen} />
             <Tab.Screen name="Search" component={SettingsScreen} />
-            <Tab.Screen name="Library" component={LibraryStackScreen} />
+            {currentUser && <Tab.Screen name="Library" component={LibraryStackScreen} />}
         </Tab.Navigator>
     );
 }

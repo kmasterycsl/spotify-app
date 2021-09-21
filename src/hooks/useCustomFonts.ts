5d59c832906316034e7f64748f16c24d4f002fe5
Khanh Tran <tranduykhanhyc@gmail.com>
2021-09-21 20:54:23 +0700

    update

--- src/hooks/useCustomFonts.ts
+++ src/hooks/useCustomFonts.ts
@@ -1,4 +1,13 @@
-import { Raleway_300Light, Raleway_300Light_Italic, Raleway_400Regular, Raleway_400Regular_Italic, Raleway_500Medium, Raleway_500Medium_Italic, Raleway_600SemiBold, Raleway_600SemiBold_Italic } from "@expo-google-fonts/raleway";
+import {
+    Raleway_300Light,
+    Raleway_300Light_Italic,
+    Raleway_400Regular,
+    Raleway_400Regular_Italic,
+    Raleway_500Medium,
+    Raleway_500Medium_Italic,
+    Raleway_600SemiBold,
+    Raleway_600SemiBold_Italic,
+} from "@expo-google-fonts/raleway";
 import { RobotoMono_400Regular, useFonts } from "@expo-google-fonts/roboto-mono";
 
 export default function useCustomFonts() {

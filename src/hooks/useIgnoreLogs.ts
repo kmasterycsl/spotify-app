1a89faa2eb7c9c2a4c54cdc4c428b0dcfb301883
Khanh Tran <tranduykhanhyc@gmail.com>
2021-09-26 22:07:25 +0700

    log

--- src/hooks/useIgnoreLogs.ts
+++ src/hooks/useIgnoreLogs.ts
@@ -4,6 +4,7 @@ import { LogBox } from "react-native";
 export default function useIgnoreLogs() {
     useEffect(() => {
         LogBox.ignoreLogs([
+            'Expected style "lineHeight: 68" to contain units',
             "ReactNativeFiberHostComponent: Calling getNode() on the ref of an Animated component is no longer necessary. You can now directly use the ref instead. This method will be removed in a future release.",
         ]);
     }, []);

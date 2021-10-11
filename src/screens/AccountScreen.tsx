dcac9c5dbb18214743accf15b7645d98b5fd33e1
Khanh Tran <tranduykhanhyc@gmail.com>
2021-10-11 22:37:07 +0700

    update

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,17 +1,21 @@
 import useLogout from "@/hooks/useLogout";
+import HorizontalPadding from "@/shared/components/HorizontalPadding";
 import SafeAreaView from "@/shared/components/SafeAreaView";
 import { GET_GENRES_QUERY } from "@/shared/queries/GET_GENRES_QUERY";
+import { useCommonStore } from "@/store/common.store";
 import { Genre, Query } from "@/types/graphql";
 import { useQuery } from "@apollo/client";
 import { useNavigation } from "@react-navigation/native";
-import { HStack, ScrollView, Text } from "native-base";
+import { HStack, Icon, ScrollView, Text } from "native-base";
 import React from "react";
 import { Dimensions, TouchableOpacity } from "react-native";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 const screenWidth = Dimensions.get("screen").width;
+import { Ionicons } from "@expo/vector-icons";
 
 export default function AccountScreen() {
     const insets = useSafeAreaInsets();
+    const currentUser = useCommonStore(store => store.currentUser);
     const nav = useNavigation();
     const { data, refetch, fetchMore } = useQuery<Query>(GET_GENRES_QUERY, {
         variables: {
@@ -31,14 +35,26 @@ export default function AccountScreen() {
     };
 
     return (
-        <SafeAreaView style={{ flex: 1 }}>
-            <ScrollView contentContainerStyle={{ alignItems: "center" }}>
-                <TouchableOpacity onPress={logout}>
-                    <HStack flexWrap="wrap" justifyContent="space-between">
-                        <Text>Log out</Text>
+        <ScrollView mt={2}>
+            <TouchableOpacity>
+                <HorizontalPadding>
+                    <HStack flexWrap="wrap" py={3} justifyContent="space-between">
+                        <Text>Your name: {currentUser?.name}</Text>
                     </HStack>
-                </TouchableOpacity>
-            </ScrollView>
-        </SafeAreaView>
+                </HorizontalPadding>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={logout}>
+                <HorizontalPadding>
+                    <HStack flexWrap="wrap" py={3} justifyContent="space-between">
+                        <Text>Sign out</Text>
+                        <Icon
+                            size="xs"
+                            color="gray.400"
+                            as={<Ionicons name="chevron-forward-outline" />}
+                        ></Icon>
+                    </HStack>
+                </HorizontalPadding>
+            </TouchableOpacity>
+        </ScrollView>
     );
 }

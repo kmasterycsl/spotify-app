821edd6be2d9f15b414980a1e77dbb981a69f6ff
Khanh Tran <tranduykhanhyc@gmail.com>
2021-10-11 22:37:41 +0700

    update

--- src/screens/AccountScreen.tsx
+++ src/screens/AccountScreen.tsx
@@ -1,38 +1,14 @@
 import useLogout from "@/hooks/useLogout";
 import HorizontalPadding from "@/shared/components/HorizontalPadding";
-import SafeAreaView from "@/shared/components/SafeAreaView";
-import { GET_GENRES_QUERY } from "@/shared/queries/GET_GENRES_QUERY";
 import { useCommonStore } from "@/store/common.store";
-import { Genre, Query } from "@/types/graphql";
-import { useQuery } from "@apollo/client";
-import { useNavigation } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
 import { HStack, Icon, ScrollView, Text } from "native-base";
 import React from "react";
-import { Dimensions, TouchableOpacity } from "react-native";
-import { useSafeAreaInsets } from "react-native-safe-area-context";
-const screenWidth = Dimensions.get("screen").width;
-import { Ionicons } from "@expo/vector-icons";
+import { TouchableOpacity } from "react-native";
 
 export default function AccountScreen() {
-    const insets = useSafeAreaInsets();
     const currentUser = useCommonStore(store => store.currentUser);
-    const nav = useNavigation();
-    const { data, refetch, fetchMore } = useQuery<Query>(GET_GENRES_QUERY, {
-        variables: {
-            page: 1,
-            limit: 9999,
-        },
-    });
-
     const logout = useLogout();
-
-    const goToDetail = (genre: Genre) => () => {
-        nav.navigate("GenreDetail", { genreId: genre.id });
-    };
-
-    const goToSearch = () => {
-        nav.navigate("Search");
-    };
 
     return (
         <ScrollView mt={2}>

557e5f8060e98bd6c09dd767067678ea52be868f
Khanh Tran <khanh.tran@canalcircle.com>
2021-10-11 22:21:48 +0700

    bsic

--- src/screens/home/home-header.tsx
+++ src/screens/home/home-header.tsx
@@ -1,15 +1,23 @@
 import { DEFAULT_HORIZONTAL_PADDING } from "@/shared/components/HorizontalPadding";
 import Ionicons from "@expo/vector-icons/build/Ionicons";
+import { useNavigation } from "@react-navigation/core";
 import { HStack, Icon, IconButton, Text } from "native-base";
 import React from "react";
 
 export default function HomeHeader() {
+    const nav = useNavigation();
+
+    const goToAccount = () => {
+        nav.navigate("Account");
+    };
+
     return (
         <HStack alignItems="center" justifyContent="space-between" px={DEFAULT_HORIZONTAL_PADDING}>
             <Text fontSize="2xl">Good evening</Text>
             <IconButton
                 size="sm"
                 variant="ghost"
+                onPress={goToAccount}
                 icon={
                     <Icon
                         size="sm"

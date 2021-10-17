9fab2371ea9e64e08ab6384f11ccdfcf9f6e04cf
Khanh Tran <khanh.tran@canalcircle.com>
2021-10-17 23:26:36 +0700

    fixed

--- src/screens/home/home-likeables.tsx
+++ src/screens/home/home-likeables.tsx
@@ -5,25 +5,17 @@ import PlaylistListItem from "@/shared/components/PlaylistListItem";
 import TracksListItem from "@/shared/components/TrackListItem";
 import VerticalPadding from "@/shared/components/VerticalPadding";
 import { GET_LIKEABLES_QUERY } from "@/shared/queries/GET_LIKEABLES_QUERY";
-import { Artist, Likeable, PaginationMeta, Playlist, Query } from "@/types/graphql";
+import { Artist, Likeable, Playlist, Query } from "@/types/graphql";
 import { useQuery } from "@apollo/client";
 import { useNavigation } from "@react-navigation/core";
 import { Box, HStack } from "native-base";
-import React, { useState } from "react";
+import React from "react";
 import { TouchableOpacity } from "react-native";
 
 export default function HomeLikeables() {
-    const [paginationMeta, setPaginationMeta] = useState<
-        Pick<PaginationMeta, "currentPage" | "totalPages">
-    >({
-        currentPage: 1,
-        totalPages: Infinity,
-    });
     const navigation = useNavigation();
 
-    const [loading, setLoading] = useState(false);
-
-    const { data, fetchMore } = useQuery<Query>(GET_LIKEABLES_QUERY, {
+    const { data } = useQuery<Query>(GET_LIKEABLES_QUERY, {
         variables: {
             page: 1,
             limit: 6,
@@ -55,24 +47,6 @@ export default function HomeLikeables() {
             <VerticalPadding />
         </Box>
     );
-
-    const onLoadMore = () => {
-        if (loading) return;
-        if (paginationMeta.currentPage >= paginationMeta.totalPages) return;
-        setLoading(true);
-        const fetched = fetchMore({
-            variables: {
-                page: paginationMeta.currentPage + 1,
-            },
-        });
-        fetched.then(({ data }) => {
-            setPaginationMeta(data.likeables.pageInfo);
-        });
-        fetched.finally(() => {
-            setLoading(false);
-        });
-        return fetched;
-    };
 
     const goToAlbum = (albumId: string) => {
         navigation.navigate("AlbumDetail", { albumId });

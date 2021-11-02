a0ce0fdb8e3e841f1329246a5c18761ffab20553
Khanh Tran <tranduykhanhyc@gmail.com>
2021-11-03 00:08:01 +0700

    fix

--- src/screens/ArtistDetailScreen.tsx
+++ src/screens/ArtistDetailScreen.tsx
@@ -11,10 +11,12 @@ import { ImageMeta, PaginationMeta, Query } from "@/types/graphql";
 import { RootStackParamList } from "@/types/routes.types";
 import { useLazyQuery, useQuery } from "@apollo/client";
 import { Ionicons } from "@expo/vector-icons";
+import { AntDesign } from "@expo/vector-icons";
+import { Entypo } from "@expo/vector-icons";
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { Box, HStack, Icon, Text, VStack } from "native-base";
 import React, { useEffect, useMemo, useState } from "react";
-import { Dimensions, StyleSheet } from "react-native";
+import { Dimensions, StyleSheet, View } from "react-native";
 import Animated, {
     useAnimatedProps,
     useAnimatedScrollHandler,
@@ -27,6 +29,9 @@ import ArtistStats from "./artist/ArtistStats";
 type ProfileScreenRouteProp = RouteProp<RootStackParamList, "ArtistDetail">;
 
 const screenWidth = Dimensions.get("screen").width;
+const HEADER_HEIGHT = 50;
+const ARTIST_NAME_HEIGHT = 35;
+const PLAY_BTN_HEIGHT = 80;
 
 export default function ArtistDetailScreen() {
     const insets = useSafeAreaInsets();
@@ -85,14 +90,17 @@ export default function ArtistDetailScreen() {
         backgroundColor: scrollOffsetY.value / screenWidth > 0.5 ? "transparent" : "gray",
     }));
 
-    const titleStyle = useAnimatedStyle(() => ({
-        top: screenWidth - 95 - scrollOffsetY.value,
+    const artistNameStyle = useAnimatedStyle(() => ({
+        top: screenWidth - ARTIST_NAME_HEIGHT - _DEFAULT_HORIZONTAL_PADDING - scrollOffsetY.value,
         opacity: 1 - (scrollOffsetY.value * 2) / screenWidth,
     }));
 
     const playBtnWrapperStyle = useAnimatedStyle(() => {
         return {
-            top: Math.max(screenWidth - 30 - scrollOffsetY.value, 52),
+            top: Math.max(
+                screenWidth + _DEFAULT_HORIZONTAL_PADDING - scrollOffsetY.value,
+                insets.top + HEADER_HEIGHT - PLAY_BTN_HEIGHT / 2
+            ),
         };
     });
 
@@ -158,40 +166,52 @@ export default function ArtistDetailScreen() {
     return data?.artist ? (
         <SafeAreaView style={styles.container} edges={["bottom"]}>
             {/* Hidden header */}
-            <Animated.View style={[styles.hiddenHeaderContainer, hiddenHeaderStyle]}>
-                <HStack
-                    style={[{ paddingTop: insets.top }, styles.hiddenHeader]}
-                    bg={(data?.artist?.coverImage?.meta as ImageMeta)?.dominantColor}
-                >
+            <Animated.View
+                style={[
+                    {
+                        height: insets.top + HEADER_HEIGHT,
+                        backgroundColor: (data?.artist?.coverImage?.meta as ImageMeta)
+                            ?.dominantColor,
+                    },
+                    styles.hiddenHeaderContainer,
+                    hiddenHeaderStyle,
+                ]}
+            >
+                <HStack style={[{ paddingTop: insets.top }, styles.hiddenHeader]}>
                     <Text>{data.artist.name}</Text>
                 </HStack>
             </Animated.View>
 
             {/* Hidden back icon */}
-            <Box style={styles.backIconContainer}>
-                <Animated.View style={[styles.backIconInner, { top: insets.top }]}>
-                    <Animated.View style={[styles.backIcon, backIconStyle]}>
-                        <Icon
-                            onPress={goBack}
-                            size={5}
-                            color="gray.400"
-                            as={<Ionicons name="chevron-back-outline" />}
-                        ></Icon>
-                    </Animated.View>
+            <Animated.View style={[styles.backIconInner, { height: insets.top + HEADER_HEIGHT }]}>
+                <Animated.View style={[{ marginTop: insets.top }, styles.backIcon, backIconStyle]}>
+                    <Icon
+                        onPress={goBack}
+                        size={5}
+                        color="gray.100"
+                        name="chevron-thin-left"
+                        as={Entypo}
+                    ></Icon>
                 </Animated.View>
-            </Box>
+            </Animated.View>
 
             {/* Cover image */}
-            <Animated.View style={[{ position: "absolute", zIndex: 0 }, coverImgStyle]}>
+            <Animated.View style={[styles.coverImgContainer, coverImgStyle]}>
                 <FullWidthSquareImage
                     url={data?.artist?.coverImage?.meta?.source}
                 ></FullWidthSquareImage>
             </Animated.View>
 
             {/* Artist name */}
-            <Animated.View style={[styles.artistName, titleStyle]}>
-                <HorizontalPadding style={{ backgroundColor: "transparent" }}>
-                    <Text fontSize="4xl" fontWeight="500" color="white">
+            <Animated.View style={[styles.artistName, artistNameStyle]}>
+                <HorizontalPadding
+                    style={{
+                        backgroundColor: "transparent",
+                        height: ARTIST_NAME_HEIGHT,
+                        justifyContent: "center",
+                    }}
+                >
+                    <Text fontSize="3xl" fontWeight="500" color="white">
                         {data.artist.name}
                     </Text>
                 </HorizontalPadding>
@@ -199,23 +219,25 @@ export default function ArtistDetailScreen() {
 
             {/* Play btn */}
             <Animated.View style={[styles.playBtnWrapper, playBtnWrapperStyle]}>
-                {isPlaying && playingArtistId === data.artist.id ? (
-                    <Icon
-                        onPress={actionPause}
-                        size={68}
-                        p={0}
-                        color="primary.400"
-                        as={<Ionicons name="pause-circle" />}
-                    ></Icon>
-                ) : (
-                    <Icon
-                        onPress={onPlay}
-                        size={68}
-                        p={0}
-                        color="primary.400"
-                        as={<Ionicons name="play-circle" />}
-                    ></Icon>
-                )}
+                <View style={[styles.playBtnWrapperInner]}>
+                    {isPlaying && playingArtistId === data.artist.id ? (
+                        <Icon
+                            onPress={actionPause}
+                            size={16}
+                            name="pausecircle"
+                            color="primary.400"
+                            as={AntDesign}
+                        ></Icon>
+                    ) : (
+                        <Icon
+                            onPress={onPlay}
+                            size={16}
+                            name="play"
+                            color="primary.400"
+                            as={AntDesign}
+                        ></Icon>
+                    )}
+                </View>
             </Animated.View>
 
             {/* Tracks list */}
@@ -231,11 +253,6 @@ export default function ArtistDetailScreen() {
                 onLoadMore={onLoadMore}
                 tracks={data.artist.tracks.items}
             />
-
-            {/* Player bar */}
-            <Box style={styles.playerBarContainer} bottom={0}>
-                {/* <PlayerBar /> */}
-            </Box>
         </SafeAreaView>
     ) : null;
 }
@@ -247,38 +264,44 @@ const styles = StyleSheet.create({
         width: "100%",
         opacity: 0,
         zIndex: 3,
+        justifyContent: "center",
+        alignItems: "center",
     },
     hiddenHeader: {
-        paddingLeft: _DEFAULT_HORIZONTAL_PADDING,
-        paddingRight: _DEFAULT_HORIZONTAL_PADDING,
-        paddingBottom: _DEFAULT_HORIZONTAL_PADDING,
         justifyContent: "center",
-        width: "100%",
-        position: "absolute",
     },
-    backIconContainer: { position: "absolute", zIndex: 3 },
-    backIconInner: { position: "absolute", left: _DEFAULT_HORIZONTAL_PADDING },
+    backIconInner: {
+        position: "absolute",
+        left: _DEFAULT_HORIZONTAL_PADDING,
+        zIndex: 3,
+        justifyContent: "center",
+    },
     backIcon: {
         backgroundColor: "gray",
         borderRadius: 100,
+        padding: 5,
     },
+    coverImgContainer: { position: "absolute", zIndex: 0 },
     artistName: {
         position: "absolute",
-        top: screenWidth - 80,
         left: 0,
         zIndex: 2,
         width: "100%",
     },
     playBtnWrapper: {
         position: "absolute",
-        top: screenWidth - 30,
-        left: screenWidth - 70,
+        right: _DEFAULT_HORIZONTAL_PADDING,
         zIndex: 3,
-        width: "100%",
+    },
+    playBtnWrapperInner: {
+        width: PLAY_BTN_HEIGHT,
+        height: PLAY_BTN_HEIGHT,
+        justifyContent: "center",
+        alignItems: "flex-end",
     },
     tracksListContainer: {
         position: "absolute",
-        paddingTop: screenWidth - 40,
+        paddingTop: screenWidth,
         top: 0,
         left: 0,
         height: "100%",
@@ -287,5 +310,4 @@ const styles = StyleSheet.create({
     tracksListFooter: {
         paddingBottom: screenWidth + 80 - 40,
     },
-    playerBarContainer: { position: "absolute", zIndex: 4, width: "100%" },
 });
